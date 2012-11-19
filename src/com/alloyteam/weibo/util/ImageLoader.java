5fe581dec0de8db5484a0a50a6d52ae272616fc4
guodayang <46741501@qq.com>
2012-11-19 20:40:20 +0800

    ~

--- src/com/alloyteam/weibo/util/ImageLoader.java
+++ src/com/alloyteam/weibo/util/ImageLoader.java
@@ -122,14 +122,14 @@ public class ImageLoader {
 
 		// Find the correct scale value. It should be the power of 2.
 		
-		 final int REQUIRED_SIZE = 70; int width_tmp = o.outWidth, height_tmp
+		 final int REQUIRED_SIZE = 2048; int width_tmp = o.outWidth, height_tmp
 		 = o.outHeight; int scale = 1; while (true) { if (width_tmp / 2 <
 		 REQUIRED_SIZE || height_tmp / 2 < REQUIRED_SIZE) break; width_tmp /=
 		 2; height_tmp /= 2; scale *= 2; } 
 		 // decode with inSampleSize
 		 BitmapFactory.Options o2 = new BitmapFactory.Options();
 		 o2.inSampleSize = scale;
-		 return BitmapFactory.decodeByteArray(bt, 0, bt.length);
+		 return BitmapFactory.decodeByteArray(bt, 0, bt.length,o2);
 		 
 
 	}
