f3a3afd2a42ead1e2df63883f7c926de94410e15
ssbs2004 <ssbs2004@163.com>
2012-11-20 11:42:45 +0800

    up

--- gen/com/alloyteam/weibo/R.java
+++ gen/com/alloyteam/weibo/R.java
@@ -8,6 +8,10 @@
 package com.alloyteam.weibo;
 
 public final class R {
+    public static final class anim {
+        public static final int head_in=0x7f040000;
+        public static final int head_out=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -28,49 +32,50 @@ public final class R {
         public static final int white=0x7f02000d;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f070009;
-        public static final int accountDelBtn=0x7f070002;
-        public static final int accountList=0x7f070006;
-        public static final int accountProviderDesc=0x7f070000;
-        public static final int addNewAccount=0x7f070005;
-        public static final int authProgressBar=0x7f070008;
-        public static final int authWebView=0x7f070007;
-        public static final int btnAccount=0x7f070019;
-        public static final int btnHomeTitlePost=0x7f07001a;
-        public static final int btnPostAddFriend=0x7f070016;
-        public static final int btnPostAddPic=0x7f070015;
-        public static final int btnPostAddTopic=0x7f070017;
-        public static final int btnPostBack=0x7f07001c;
-        public static final int btnPostSave=0x7f07001d;
-        public static final int btn_account_manager=0x7f07000b;
-        public static final int btn_group=0x7f07000d;
-        public static final int btn_post=0x7f07000c;
-        public static final int descTextView=0x7f070001;
-        public static final int editText1=0x7f070018;
-        public static final int etPostMain=0x7f070012;
-        public static final int imageView1=0x7f070021;
-        public static final int imageView2=0x7f070024;
-        public static final int imageView3=0x7f070028;
-        public static final int listView1=0x7f070010;
-        public static final int lv_main_timeline=0x7f07000e;
-        public static final int menu_settings=0x7f07002a;
-        public static final int postStatebar=0x7f070013;
-        public static final int providerDesc=0x7f070003;
-        public static final int providerSelect=0x7f070004;
-        public static final int relativeLayout1=0x7f070025;
-        public static final int tab_item_icon=0x7f07001f;
-        public static final int tab_item_text=0x7f070020;
-        public static final int tabsLayout=0x7f07001e;
-        public static final int textView1=0x7f070011;
-        public static final int textView2=0x7f070022;
-        public static final int textView3=0x7f070023;
-        public static final int textView4=0x7f070026;
-        public static final int textView5=0x7f070027;
-        public static final int textView6=0x7f070029;
-        public static final int tvPostCount=0x7f070014;
-        public static final int tvTitle=0x7f07001b;
-        public static final int tv_home_loading=0x7f07000f;
-        public static final int tv_home_title=0x7f07000a;
+        public static final int RelativeLayout1=0x7f080009;
+        public static final int accountDelBtn=0x7f080002;
+        public static final int accountList=0x7f080006;
+        public static final int accountProviderDesc=0x7f080000;
+        public static final int addNewAccount=0x7f080005;
+        public static final int authProgressBar=0x7f080008;
+        public static final int authWebView=0x7f080007;
+        public static final int bigImage=0x7f080010;
+        public static final int btnAccount=0x7f08001a;
+        public static final int btnHomeTitlePost=0x7f08001b;
+        public static final int btnPostAddFriend=0x7f080017;
+        public static final int btnPostAddPic=0x7f080016;
+        public static final int btnPostAddTopic=0x7f080018;
+        public static final int btnPostBack=0x7f08001d;
+        public static final int btnPostSave=0x7f08001e;
+        public static final int btn_account_manager=0x7f08000b;
+        public static final int btn_group=0x7f08000d;
+        public static final int btn_post=0x7f08000c;
+        public static final int descTextView=0x7f080001;
+        public static final int editText1=0x7f080019;
+        public static final int etPostMain=0x7f080013;
+        public static final int imageView1=0x7f080022;
+        public static final int imageView2=0x7f080027;
+        public static final int listView1=0x7f080011;
+        public static final int lv_main_timeline=0x7f08000e;
+        public static final int menu_settings=0x7f08002b;
+        public static final int postStatebar=0x7f080014;
+        public static final int providerDesc=0x7f080003;
+        public static final int providerSelect=0x7f080004;
+        public static final int relativeLayout1=0x7f080026;
+        public static final int tab_item_icon=0x7f080020;
+        public static final int tab_item_text=0x7f080021;
+        public static final int tabsLayout=0x7f08001f;
+        public static final int textView1=0x7f080012;
+        public static final int textView2=0x7f080023;
+        public static final int textView3=0x7f080024;
+        public static final int textView4=0x7f080028;
+        public static final int textView5=0x7f080029;
+        public static final int textView6=0x7f08002a;
+        public static final int thumbImage=0x7f080025;
+        public static final int tvPostCount=0x7f080015;
+        public static final int tvTitle=0x7f08001c;
+        public static final int tv_home_loading=0x7f08000f;
+        public static final int tv_home_title=0x7f08000a;
     }
     public static final class layout {
         public static final int account_manager_item=0x7f030000;
@@ -92,30 +97,38 @@ public final class R {
         public static final int weibo_item_type2=0x7f030010;
     }
     public static final class menu {
-        public static final int activity_auth=0x7f060000;
-        public static final int activity_login=0x7f060001;
-        public static final int activity_main=0x7f060002;
+        public static final int activity_auth=0x7f070000;
+        public static final int activity_login=0x7f070001;
+        public static final int activity_main=0x7f070002;
     }
     public static final class string {
-        public static final int account_manager=0x7f040004;
-        public static final int add_account=0x7f040009;
-        public static final int app_name=0x7f040000;
-        public static final int btn_add_friend=0x7f04000e;
-        public static final int btn_add_pic=0x7f04000d;
-        public static final int btn_add_topic=0x7f04000f;
-        public static final int btn_back=0x7f04000b;
-        public static final int btn_save=0x7f04000c;
-        public static final int ensure_text=0x7f04000a;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int post=0x7f040006;
-        public static final int time_line=0x7f040005;
-        public static final int title_activity_auth=0x7f040007;
-        public static final int title_activity_login=0x7f040008;
-        public static final int title_activity_main=0x7f040003;
+        public static final int account_manager=0x7f050004;
+        public static final int add_account=0x7f05000a;
+        public static final int app_name=0x7f050000;
+        public static final int btn_add_friend=0x7f05000f;
+        public static final int btn_add_pic=0x7f05000e;
+        public static final int btn_add_topic=0x7f050010;
+        public static final int btn_back=0x7f05000c;
+        public static final int btn_save=0x7f05000d;
+        public static final int ensure_text=0x7f05000b;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int post=0x7f050006;
+        public static final int time_line=0x7f050005;
+        public static final int title_activity_auth=0x7f050007;
+        public static final int title_activity_image=0x7f050008;
+        public static final int title_activity_login=0x7f050009;
+        public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
-        public static final int homeTitleBackground=0x7f050001;
+        public static final int AnimHead=0x7f060002;
+        public static final int AppTheme=0x7f060000;
+        /**  模糊 
+         */
+        public static final int ImageScale=0x7f060003;
+        /**  背景是否模糊 
+         */
+        public static final int ImageloadingDialogStyle=0x7f060004;
+        public static final int homeTitleBackground=0x7f060001;
     }
 }
